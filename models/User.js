7a1746ae7d5db1e0e6c373bb7c5e8f045ea66749
Leon-kings <leonakingeneye2002@gmail.com>
2025-12-02 15:56:47 +0200

    changes on user models

--- models/User.js
+++ models/User.js
@@ -1,3 +1,77 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const validator = require('validator');
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: [true, 'Name is required'],
+//       trim: true,
+//       maxlength: [50, 'Name cannot be more than 50 characters'],
+//     },
+//     email: {
+//       type: String,
+//       required: [true, 'Email is required'],
+//       unique: true,
+//       lowercase: true,
+//       validate: [validator.isEmail, 'Please provide a valid email'],
+//     },
+//     password: {
+//       type: String,
+//       required: [true, 'Password is required'],
+//       minlength: [6, 'Password must be at least 6 characters'],
+//       select: false, // don't return password by default
+//     },
+//     status: {
+//       type: String,
+//       enum: ['user', 'admin'],
+//       default: 'user',
+//     },
+//     isVerified: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     lastLogin: {
+//       type: Date,
+//     },
+//     verificationToken: String,
+//     resetPasswordToken: String,
+//     resetPasswordExpire: Date,
+//   },
+//   { timestamps: true }
+// );
+
+// // Hash password before saving
+// userSchema.pre('save', async function (next) {
+//   try {
+//     // Only hash if password is modified
+//     if (!this.isModified('password')) return next();
+
+//     this.password = await bcrypt.hash(this.password, 12);
+//     next();
+//   } catch (err) {
+//     next(err); // pass errors to next middleware
+//   }
+// });
+
+// // Compare password
+// userSchema.methods.comparePassword = async function (candidatePassword) {
+//   if (!this.password) throw new Error('Password not set for this user');
+//   return await bcrypt.compare(candidatePassword, this.password);
+// };
+
+// // Remove sensitive fields when returning user object
+// userSchema.methods.toJSON = function () {
+//   const user = this.toObject();
+//   delete user.password;
+//   delete user.verificationToken;
+//   delete user.resetPasswordToken;
+//   delete user.resetPasswordExpire;
+//   return user;
+// };
+
+// module.exports = mongoose.model('User', userSchema);
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
@@ -21,7 +95,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Password is required'],
       minlength: [6, 'Password must be at least 6 characters'],
-      select: false, // don't return password by default
+      select: false,
     },
     status: {
       type: String,
@@ -32,9 +106,7 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    lastLogin: {
-      type: Date,
-    },
+    lastLogin: Date,
     verificationToken: String,
     resetPasswordToken: String,
     resetPasswordExpire: Date,
@@ -43,22 +115,16 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  try {
-    // Only hash if password is modified
-    if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-    this.password = await bcrypt.hash(this.password, 12);
-    next();
-  } catch (err) {
-    next(err); // pass errors to next middleware
-  }
+  this.password = await bcrypt.hash(this.password, 12);
 });
 
 // Compare password
 userSchema.methods.comparePassword = async function (candidatePassword) {
   if (!this.password) throw new Error('Password not set for this user');
-  return await bcrypt.compare(candidatePassword, this.password);
+  return bcrypt.compare(candidatePassword, this.password);
 };
 
 // Remove sensitive fields when returning user object
